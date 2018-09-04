38df04ba898e6cdd5aa39b3476f3bf698253ee6b
grzegorz <grzegorz.krzeszowski@sky-shop.pl>
2018-09-04 23:10:57 +0200

    add project

---
+++ src/AppBundle/Controller/PersonInformationController.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace AppBundle\Controller;
+
+
+use AppBundle\Entity\Person;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class PersonInformationController extends Controller
+{
+	/**
+	 * @Route("/", name="homepage")
+	 */
+	public function personAction()
+	{
+		return $this->render("personInformation/index.html.twig", [
+
+		]);
+	}
+
+	/**
+	 * @Route("/save")
+	 */
+	public function personSaveAction(Request $request)
+	{
+		$responseMsg = 'Dane były nieprawidłowo wypełnione';
+
+		$firstName = $request->request->get('firstname');
+		$surname = $request->request->get('surname');
+
+		if (!empty($firstName) && !empty($surname))
+		{
+			$entityManager = $this->getDoctrine()->getManager();
+
+			$person = new Person();
+			$person->setFirstname($firstName);
+			$person->setSurname($surname);
+
+			$entityManager->persist($person);
+			$entityManager->flush();
+
+			$responseMsg = "Zapisano nowy rekord w bazie";
+		}
+
+		return new Response($responseMsg);
+
+	}
+
+}
---
+++ src/AppBundle/Entity/Person.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Person
+ *
+ * @ORM\Table(name="person")
+ * @ORM\Entity(repositoryClass="AppBundle\Repository\PersonRepository")
+ */
+class Person
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="firstname", type="string", length=63)
+     */
+    private $firstname;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="surname", type="string", length=63)
+     */
+    private $surname;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set firstname
+     *
+     * @param string $firstname
+     *
+     * @return Person
+     */
+    public function setFirstname($firstname)
+    {
+        $this->firstname = $firstname;
+
+        return $this;
+    }
+
+    /**
+     * Get firstname
+     *
+     * @return string
+     */
+    public function getFirstname()
+    {
+        return $this->firstname;
+    }
+
+    /**
+     * Set surname
+     *
+     * @param string $surname
+     *
+     * @return Person
+     */
+    public function setSurname($surname)
+    {
+        $this->surname = $surname;
+
+        return $this;
+    }
+
+    /**
+     * Get surname
+     *
+     * @return string
+     */
+    public function getSurname()
+    {
+        return $this->surname;
+    }
+}
+
